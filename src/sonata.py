5aedb17d7e69ec034df7ac73cc613e0f31e577f9
Han Zhou <zhouhan96@foxmail.com>
2023-11-14 15:36:05 +0800

    update notebook for circle datasets

--- src/sonata.py
+++ src/sonata.py
@@ -158,7 +158,7 @@ class sonata(object):
 
         cell_vec_aug = cell_amat_safe[:n, n]
         cell_amat_safe = cell_amat_safe[:n, :n]
-        # Ambiguous value should not be higher than baseline(cell_vec_aug)
+        # Ambiguous value should not be higher than baseline (cell_vec_aug)
         for node_idx in range(n):
             cell_amat_safe[node_idx, :] = np.maximum(cell_amat_safe[node_idx, :], cell_vec_aug[node_idx])
         return cell_amat_safe
@@ -185,7 +185,7 @@ class sonata(object):
             # fitting spline 
             spline = UnivariateSpline(X_arr, Y_arr, k = 4) 
 
-            # find minimal of curve
+            # find minimal inflection point of the curve
             dv1 = spline.derivative(n = 1) # 1st derivative
             y_dv1 = dv1(X_arr)
             tck = splrep(X_arr, y_dv1)
@@ -193,7 +193,7 @@ class sonata(object):
             dv1_roots = ppoly.roots(extrapolate=False) # 1st derivative = 0
             dv2 = spline.derivative(n = 2) # 2nd derivative
 
-            # remove ambiguous neighbors by 1st curve min for ambiguity
+            # remove ambiguous neighbors by detecting 1st minimal inflection point
             curve_m = np.where(dv2(dv1_roots) > 0)[0]
 
             if len(curve_m) > 0:
@@ -214,7 +214,7 @@ class sonata(object):
             map_mat = None
         else:
             ambiguous_node_groups = self.find_ambiguous_groups(data, ambiguous_nodes)
-            map_mat = self.map_uncertain_groups(data, ambiguous_node_groups, unambiguous_nodes)
+            map_mat = self.map_ambiguous_groups(data, ambiguous_node_groups, unambiguous_nodes)
 
         return map_mat
         
@@ -236,7 +236,7 @@ class sonata(object):
         data_mat_ambiguous = data[ambiguous_nodes, :]
         cell_amat_ambiguous = self.cell_amat[ambiguous_nodes, :][:, ambiguous_nodes]
 
-        # uncertainty threshold
+        # ambiguity threshold
         ambiguous_indices = np.where(cell_amat_ambiguous > self.t)
         
         # all cell-wise ambiguity
@@ -291,7 +291,7 @@ class sonata(object):
         self.K_yerror = y_error
         self.K_xstep = x_step
 
-    def map_uncertain_groups(self, data, ambiguous_node_groups, unambiguous_nodes):
+    def map_ambiguous_groups(self, data, ambiguous_node_groups, unambiguous_nodes):
         np.seterr(under='ignore')
         ambiguous_nodes = np.asarray([], dtype=int)
         for group in ambiguous_node_groups:
@@ -300,7 +300,7 @@ class sonata(object):
 
         ### evaluate valid group
         if self.eval_knn:
-            valid_perm = eval_valid_group_knn(ambiguous_node_groups, unambiguous_nodes, self.knn)
+            valid_perm = self.eval_valid_group_knn(ambiguous_node_groups, unambiguous_nodes)
         else:
             valid_perm= list(itertools.permutations(range(len(ambiguous_node_groups))))[1:]
         print("all vaild perms are: ", valid_perm)
@@ -311,7 +311,7 @@ class sonata(object):
             map_mat = np.zeros_like(self.geo_mat)
             print("perms: ", perms)
 
-            # keep the diagonal for certain nodes
+            # keep the diagonal for unambiguous nodes
             for node in unambiguous_nodes: map_mat[node, node] = 1.0        
 
             for i in range(len(ambiguous_node_groups)):
@@ -354,41 +354,41 @@ class sonata(object):
 
         return second_grad
 
-def eval_valid_group_knn(ambiguous_node_groups, unambiguous_nodes, knn):
-    knn = knn.tocoo()
-    knn_arr = knn.toarray()
+    def eval_valid_group_knn(self, ambiguous_node_groups, unambiguous_nodes):
+        knn = self.knn.tocoo()
+        knn_arr = knn.toarray()
 
-    connected_groups = []
-    for tup in list(itertools.combinations(range(len(ambiguous_node_groups)), 2)):
-        group_idx1, group_idx2 = tup[0], tup[1]
-        node_group1 = ambiguous_node_groups[group_idx1]
-        node_group2 = ambiguous_node_groups[group_idx2]
-        sub_knn_arr = knn_arr[node_group1, :][:, node_group2]
-        if np.sum(sub_knn_arr) > 0.0:
-            connected_groups.append(tup)
-    print("connected_groups_uncertain: ", connected_groups)
+        connected_groups = []
+        for tup in list(itertools.combinations(range(len(ambiguous_node_groups)), 2)):
+            group_idx1, group_idx2 = tup[0], tup[1]
+            node_group1 = ambiguous_node_groups[group_idx1]
+            node_group2 = ambiguous_node_groups[group_idx2]
+            sub_knn_arr = knn_arr[node_group1, :][:, node_group2]
+            if np.sum(sub_knn_arr) > 0.0:
+                connected_groups.append(tup)
+        print("connected_groups_ambiguous: ", connected_groups)
 
-    for group_idx1 in range(len(ambiguous_node_groups)):
-        group_idx2 = -1 # certain nodes
-        node_group1 = ambiguous_node_groups[group_idx1]
-        sub_knn_arr = knn_arr[node_group1, :][:, unambiguous_nodes]
-        if np.sum(sub_knn_arr) > 0:
-            connected_groups.append((group_idx1, group_idx2))
-    connected_groups = np.array(connected_groups)
-    print('connected_groups_all: ', connected_groups)   
-    
-    valid_perm = []
-    sorted_connected_groups = np.sort(connected_groups, axis = 1)
-    for perms in list(itertools.permutations(range(len(ambiguous_node_groups))))[1:]:
-        new_connected_groups = np.copy(connected_groups)
-        for i in range(len(ambiguous_node_groups)):
-            group_idx1 = i
-            group_idx2 = perms[i]
-            if group_idx1 != group_idx2:
-                new_connected_groups[connected_groups == group_idx1] = group_idx2  
-        sorted_new_connected_groups = np.sort(new_connected_groups, axis = 1)  
+        for group_idx1 in range(len(ambiguous_node_groups)):
+            group_idx2 = -1 # unambiguous nodes
+            node_group1 = ambiguous_node_groups[group_idx1]
+            sub_knn_arr = knn_arr[node_group1, :][:, unambiguous_nodes]
+            if np.sum(sub_knn_arr) > 0:
+                connected_groups.append((group_idx1, group_idx2))
+        connected_groups = np.array(connected_groups)
+        print('connected_groups_all: ', connected_groups)   
+        
+        valid_perm = []
+        sorted_connected_groups = np.sort(connected_groups, axis = 1)
+        for perms in list(itertools.permutations(range(len(ambiguous_node_groups))))[1:]:
+            new_connected_groups = np.copy(connected_groups)
+            for i in range(len(ambiguous_node_groups)):
+                group_idx1 = i
+                group_idx2 = perms[i]
+                if group_idx1 != group_idx2:
+                    new_connected_groups[connected_groups == group_idx1] = group_idx2  
+            sorted_new_connected_groups = np.sort(new_connected_groups, axis = 1)  
 
-        if sorted(sorted_connected_groups.tolist()) == sorted(sorted_new_connected_groups.tolist()):
-            valid_perm.append(perms)
+            if sorted(sorted_connected_groups.tolist()) == sorted(sorted_new_connected_groups.tolist()):
+                valid_perm.append(perms)
 
-    return valid_perm
+        return valid_perm
